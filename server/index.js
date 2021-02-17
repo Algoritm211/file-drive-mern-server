08832693ddcba1e5d4d14f26185acd4c3e1a1809
alexey_horbunov <algoritm211@gmail.com>
2021-02-18 00:51:29 +0200

    Add authentication on server

--- server/index.js
+++ server/index.js
@@ -2,23 +2,26 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('config')
 const authRouter = require('./routes/api.routes')
+const cors = require('cors');
 
 const app = express()
 const PORT = config.get('serverPORT')
 const dbURL = config.get('dbURL')
 
+app.use(cors());
 app.use(express.json())
 app.use('/api/auth/', authRouter)
 
 const startApp = async () => {
   try {
+
     await mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
 
     app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}`)
     })
   } catch (error) {
-    // console.log(error)
+    console.log(error)
   }
 }
 
